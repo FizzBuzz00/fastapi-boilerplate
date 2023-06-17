24d69bc3590a5bcb0290af746febe29140b36071
Arup <arupmamushev100@gmail.com>
2023-06-17 17:25:33 +0600

    Check

--- app/shanyrak/service.py
+++ app/shanyrak/service.py
@@ -1,6 +1,6 @@
 
 from app.config import database
-# from .adapters.s3_service import S3Service
+from .adapters.s3_service import S3Service
 
 from .repository.repository import ShanyrakRepository
 
@@ -10,12 +10,12 @@ class Service:
     def __init__(
         self,
         repository: ShanyrakRepository,
-        # s3_service: S3Service
+        s3_service: S3Service
     ):
         self.repository = repository
-        # self.s3_service = s3_service
+        self.s3_service = s3_service
 
 def get_service():
     repository = ShanyrakRepository(database)
-    # service  = S3Service()
-    return Service(repository)
+    service  = S3Service()
+    return Service(repository, service)
